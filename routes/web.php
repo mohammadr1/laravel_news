52774d2d8cd5b20fdb17294d13f61550fe553823
mohammadrahimi4662 <mohammadrahimi4662@gmail.com>
2025-05-05 10:25:50 +0330

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Customer\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 
 Route::get('/', [HomeController::class, 'home'])->name('customer.home');
  
-Route::namespace('Auth')->group(function(){
-    Route::get('/login-register', [LoginRegisterController::class, 'loginRegisterForm'])->name('auth.customer.login-register-form');
-    Route::middleware('throttle:customer-login-register-limiter')->post('/login-register', [LoginRegisterController::class, 'loginRegister'])->name('auth.customer.login-register');
-    
-    Route::get('login-confirm/{token}', [LoginRegisterController::class, 'loginConfirmForm'])->name('auth.customer.login-confirm-form');
-    Route::middleware('throttle:customer-login-confirm-limiter')->post('/login-confirm/{token}', [LoginRegisterController::class, 'loginConfirm'])->name('auth.customer.login-confirm');
-    
-    Route::middleware('throttle:customer-login-resend-otp-limiter')->get('/login-resend-otp/{token}', [LoginRegisterController::class, 'loginResendOtp'])->name('auth.customer.login-resend-otp');
-    
-    Route::get('/logout', [LoginRegisterController::class, 'logout'])->name('auth.customer.logout');
-});
-
 
 Route::prefix('admin')->namespace('Admin')->group(function(){
     Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
